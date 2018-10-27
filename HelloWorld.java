73a2cd03ded8663aaf495f3a9beb3d1672a1cdfe
professorwcm <44531474+professorwcm@users.noreply.github.com>
2018-10-27 14:02:38 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ *
+ *
+ * @author Will McLaughlin
+ * @version 10.27.18.14:04
+ */
+public class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Will was here!");
+		
+	}
+}
